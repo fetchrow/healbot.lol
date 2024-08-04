97a26787b50ab596fa214ee308323a9876e08cfa
1deals <1dealsdiscord@gmail.com>
2024-08-04 13:04:26 +0000

    lol

--- tools/heal.py
+++ tools/heal.py
@@ -5,6 +5,8 @@ import asyncpg
 import asyncio
 import aiohttp
 import discord
+import secrets
+import string
 import glob
 import json
 import sys
@@ -82,8 +84,61 @@ class Heal(commands.Bot):
     async def get_context(self, message: Message, *, cls=Context):
         return await super().get_context(message, cls=cls)
     
-    async def on_command_error(self, ctx: Context, ex: commands.CommandError):
-        if isinstance(ex, commands.errors.NotOwner):
-            return await ctx.deny(f'You are not an owner of {self.user.mention}.')
-        if isinstance(ex, commands.errors.CommandOnCooldown):
-            return await ctx.warn(f'This command is currently on cooldown, try again in {int(ex.retry_after)}s')
+    async def on_command_error(self, ctx: Context, exception: commands.CommandError) -> None:
+        if type(exception) in [commands.CommandNotFound, commands.NotOwner, commands.CheckFailure]: return
+        elif isinstance(exception, commands.BadColourArgument):
+            return await ctx.warn(f"I was **unable** to find that **color**.")
+        elif isinstance(exception, commands.RoleNotFound):
+            return await ctx.warn(f"I was unable to find the role **{exception.argument}**.")
+        elif isinstance(exception, commands.ChannelNotFound):
+            return await ctx.warn(f"I was unable to find the channel **{exception.argument}**")
+        elif isinstance(exception, commands.ThreadNotFound):
+            return await ctx.warn(f"I was unable to find the thread **{exception.argument}**")
+        elif isinstance(exception, commands.BadUnionArgument):
+            if discord.Emoji in exception.converters or discord.PartialEmoji in exception.converters:
+                return await ctx.warn(f"Invalid **emoji** provided.")
+            elif discord.User in exception.converters or discord.Member in exception.converters:
+                return await ctx.warn(f"I was unable to find that **member** or the provided **ID** is invalid.")
+            return await ctx.warn(f"Could not convert **{exception.param.name}** to **{exception.converters}**")
+        elif isinstance(exception, commands.CommandInvokeError):
+            if isinstance(exception.original, ValueError):
+                return await ctx.warn(exception.original)
+            elif isinstance(exception.original, discord.HTTPException):
+                return await ctx.warn(f"**Invalid code**\n```{exception.original}```")
+            elif isinstance(exception.original, aiohttp.ClientConnectorError):
+                return await ctx.warn(f"**Failed** to connect to the **URL** - Possibly invalid.")
+            elif isinstance(exception.original, aiohttp.ClientResponseError): 
+                if exception.original.status == 522:
+                    return await ctx.warn(f"**Timed out** while requesting data - probably the API's fault")
+                return await ctx.warn(f"**API** returned a **{exception.original.status}** status - try again later.")
+            elif isinstance(exception.original, discord.Forbidden):
+                return await ctx.warn(f"I'm **missing** permission: `{exception.original.missing_perms[0]}`")
+            elif isinstance(exception.original, discord.NotFound):
+                return await ctx.warn(f"**Not found** - the **ID** is invalid")
+            elif isinstance(exception.original, aiohttp.ContentTypeError):
+                return await ctx.warn(f"**Invalid content** - the **API** returned an unexpected response")
+            elif isinstance(exception.original, aiohttp.InvalidURL):
+                return await ctx.warn(f"The provided **url** is invalid")
+            elif isinstance(exception.original, asyncpg.StringDataRightTruncationError):
+                return await ctx.warn(f"**Data** is too **long** - try again with a shorter message")
+            return await ctx.warn(exception.original)
+        elif isinstance(exception, commands.UserNotFound):
+            return await ctx.warn("I was unable to find that **member** or the **ID** is invalid")
+        elif isinstance(exception, commands.MemberNotFound):
+            return await ctx.warn(f"I was unable to find a member with the name: **{exception.argument}**")
+        elif isinstance(exception, commands.MissingPermissions):
+            return await ctx.warn(f"You're **missing** permission: `{exception.missing_permissions[0]}`")
+        elif isinstance(exception, commands.BotMissingPermissions):
+            return await ctx.warn(f"I'm **missing** permission: `{exception.missing_permissions[0]}`")
+        elif isinstance(exception, commands.GuildNotFound):
+            return await ctx.warn(f"I was unable to find that **server** or the **ID** is invalid")
+        elif isinstance(exception, commands.BadInviteArgument):
+            return await ctx.warn(f"Invalid **invite code** given")
+        elif isinstance(exception, commands.UserInputError): 
+            return await ctx.warn(f"**Invalid Input Given**: \n`{exception}`")
+        elif isinstance(exception, commands.CommandOnCooldown):
+            return await ctx.neutral(f"Please wait **{exception.retry_after:.2f} seconds** before using any command again.", emoji=self.config.EMOJI.DEFAULT.COOLDOWN, delete_after=2)
+        
+        code = ''.join(secrets.choice(string.ascii_letters + string.digits) for _ in range(13))
+        self.errors[code] = exception
+        return await ctx.warn(message=f"Error occurred whilst performing command **{ctx.command.qualified_name}**. Use the given error code to report it to the developers in the [support server](https://discord.gg/tCZDT7YdUF)", content=f"`{code}`")  
